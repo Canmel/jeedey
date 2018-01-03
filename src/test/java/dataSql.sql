5eb8d0e2e2a1c2c3a00c909bbdb7c6a4225fb95a
baily <lidj@c-sunrise.com>
2018-01-03 22:31:32 +0800

    修改dataSql.sql

--- src/test/java/dataSql.sql
+++ src/test/java/dataSql.sql
@@ -3,6 +3,6 @@ insert into users(name, phone, email, status) values("Tom", "13987878787", "Tom@
 insert into users(name, phone, email, status) values("Jack", "13987878789", "Jack@test.com", 1);
 insert into users(name, phone, email, status) values("Marry", "13987878788", "Marry@test.com", 1);
 insert into users(name, phone, email, status) values("Jim", "13987878786", "Jim@test.com", 1);
-insert into users(name, phone, email, status) values("Tony", "13987878785", "Jim@test.com", 0);
-insert into users(name, phone, email, status) values("Paul", "13987878784", "Jim@test.com", 0);
-insert into users(name, phone, email, status) values("Ama", "13987878783", "Jim@test.com", 0)
+insert into users(name, phone, email, status) values("Tony", "13987878785", "Tony@test.com", 0);
+insert into users(name, phone, email, status) values("Paul", "13987878784", "Paul@test.com", 0);
+insert into users(name, phone, email, status) values("Ama", "13987878783", "Ama@test.com", 0)
