92bfaf40185b0ed7ec451b118579f47072b5fb33
baily <lidj@c-sunrise.com>
2018-01-04 14:53:03 +0800

    设置404界面

--- src/main/java/com/meedesidy/jeedey/interceptor/exceptions/FinalExceptionHandler.java
+++ src/main/java/com/meedesidy/jeedey/interceptor/exceptions/FinalExceptionHandler.java
@@ -1,28 +1,25 @@
 package com.meedesidy.jeedey.interceptor.exceptions;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-public class FinalExceptionHandler implements ErrorController{
+@ControllerAdvice
+public class FinalExceptionHandler{
 
 	private static final String ERROR_PAGE = "error"; 
 	
-	@Override
-	public String getErrorPath() {
-		// TODO Auto-generated method stub
-		return ERROR_PAGE;
-	}
+	@ExceptionHandler(Exception.class)
+    public ModelAndView error(Exception e) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName(ERROR_PAGE);
+        mv.setStatus(HttpStatus.NOT_FOUND);
+        return mv;
+    }
 	
-	@RequestMapping(value = ERROR_PAGE)
-    public ModelAndView error(HttpServletResponse resp, HttpServletRequest req) {
-		System.out.println("222222222222222222");
+	@ExceptionHandler(RuntimeException.class)
+    public ModelAndView runtime(Exception e) {
         ModelAndView mv = new ModelAndView();
         mv.setViewName(ERROR_PAGE);
         mv.setStatus(HttpStatus.NOT_FOUND);
