393a996fab0dd6966fc2eff819c7d8a8af65dd54
baily <lidj@c-sunrise.com>
2018-01-04 10:47:07 +0800

    环境调整

--- docker_web_run.sh
+++ docker_web_run.sh
@@ -2,5 +2,5 @@ echo "welcome to docker_web_run"
 
 source /etc/environment
 
-java -jar /tmp/app/target/spring-application-webapp-boot.jar
+java -jar /tmp/app/target/spring-application-webapp-boot.jar --spring.profiles.active=prod
 /bin/bash
